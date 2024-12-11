8d61b96c533ea2ccd72dafee555da7e7ef6b0be7
jie47 <91864160+jie47@users.noreply.github.com>
2024-12-11 20:55:12 +0800

    Added URL Encoding for the Password

--- auto.py
+++ auto.py
@@ -1,14 +1,16 @@
 import time
+from urllib.parse import quote
 from portal import login, unbind, logout
 
 # 配置
 username = '8123456789' # 学号
 password = 'xxxxxxxxxx' # 密码
 type = '中国移动' # 中国移动、中国联通、中国电信、校园网
+encoded_password = quote(password)
 
 # 先解绑后自动登录
 unbind(username=username)
 time.sleep(3)
 logout()
 time.sleep(3)
-login(username=username, password=password, type=type)
+login(username=username, password=encoded_password, type=type)
