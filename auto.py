a0d1dc2fede9360de529de6bf33ee46a44e651ef
barkure <43804451+barkure@users.noreply.github.com>
2025-01-10 18:08:25 +0800

    chore: Update .gitignore and refactor login functions for improved readability and parameter handling

--- auto.py
+++ auto.py
@@ -1,16 +1,14 @@
 import time
-from urllib.parse import quote
 from portal import login, unbind, logout
 
 # 配置
-username = '8123456789' # 学号
-password = 'xxxxxxxxxx' # 密码
-type = '中国移动' # 中国移动、中国联通、中国电信、校园网
-encoded_password = quote(password)
+username = "8211211214"  # 学号
+password = "qin070809"  # 密码
+type = "中国联通"  # 中国移动、中国联通、中国电信、校园网
 
 # 先解绑后自动登录
 unbind(username=username)
 time.sleep(3)
 logout()
 time.sleep(3)
-login(username=username, password=encoded_password, type=type)
+login(username=username, password=password, type=type)
--- portal.py
+++ portal.py
@@ -1,46 +1,59 @@
 import requests
+from urllib.parse import quote
+
 
 # 加载页面设置信息
 def load_config():
-    url = 'https://portal.csu.edu.cn:802/eportal/portal/page/loadConfig'
+    url = "https://portal.csu.edu.cn:802/eportal/portal/page/loadConfig"
     response = requests.get(url)
     print(response.text)
+
 
 # 检查状态
 def check_status():
-    dr = ''
-    url = f'https://portal.csu.edu.cn/drcom/chkstatus?callback={dr}'
-    response = requests.get(url)
+    dr = ""
+    url = "https://portal.csu.edu.cn/drcom/chkstatus"
+    params = {"callback": dr}
+    response = requests.get(url, params=params)
     print(response.text)
+
 
 # 在线数据
-def online_data(username, password):
-    url = f'https://portal.csu.edu.cn:802/eportal/portal/Custom/online_data?username={username}&password={password}'
-    response = requests.get(url)
+def online_data(username: str, password: str):
+    url = "https://portal.csu.edu.cn:802/eportal/portal/Custom/online_data"
+    encoded_password = quote(password)
+    params = {"username": username, "password": encoded_password}
+    response = requests.get(url, params=params)
     print(response.text)
+
 
 # 登录认证
-def login(username, password, type):
+def login(username: str, password: str, type: str):
     net_types = {
-        '中国电信': 'telecomn',
-        '中国移动': 'cmccn',
-        '中国联通': 'unicomn',
-        '校园网': ''
+        "中国电信": "telecomn",
+        "中国移动": "cmccn",
+        "中国联通": "unicomn",
+        "校园网": "",
     }
-    user_account = username + '@' + net_types[type]
+    user_account = username + "@" + net_types[type]
     print("登陆账户：", user_account)
-    url = f'https://portal.csu.edu.cn:802/eportal/portal/login?user_account={user_account}&user_password={password}'
-    response = requests.get(url)
+    url = "https://portal.csu.edu.cn:802/eportal/portal/login"
+    encoded_password = quote(password)
+    params = {"user_account": user_account, "user_password": encoded_password}
+    response = requests.get(url, params=params)
     print(response.text)
 
+
 # 解绑
-def unbind(username):
-    url = f'https://portal.csu.edu.cn:802/eportal/portal/mac/unbind?user_account={username}'
-    response = requests.get(url)
+def unbind(username: int):
+    url = "https://portal.csu.edu.cn:802/eportal/portal/mac/unbind"
+    params = {"user_account": username}
+    response = requests.get(url, params=params)
     print(response.text)
+
 
 # 退出
 def logout():
-    url = 'https://portal.csu.edu.cn:802/eportal/portal/logout'
+    url = "https://portal.csu.edu.cn:802/eportal/portal/logout"
     response = requests.get(url)
     print(response.text)
