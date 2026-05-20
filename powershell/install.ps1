cefc2721a2e2c7be6cdadad7af0d5ec1c74e148f
barkure <43804451+barkure@users.noreply.github.com>
2026-05-20 16:41:06 +0800

    Fix PowerShell installer prompt execution

--- powershell/install.ps1
+++ powershell/install.ps1
@@ -5,12 +5,21 @@ $InstallerUrl = "$RepoRawUrl/powershell/common/install-main.ps1"
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 $webClient = New-Object System.Net.WebClient
+$tempPath = Join-Path ([System.IO.Path]::GetTempPath()) ("csu-autoauth-install-{0}.ps1" -f ([System.Guid]::NewGuid().ToString("N")))
 
 try {
     $scriptBytes = $webClient.DownloadData($InstallerUrl)
+    $installerScript = [System.Text.Encoding]::UTF8.GetString($scriptBytes)
+    $utf8Bom = New-Object System.Text.UTF8Encoding($true)
+    [System.IO.File]::WriteAllText($tempPath, $installerScript, $utf8Bom)
 } finally {
     $webClient.Dispose()
 }
 
-$installerScript = [System.Text.Encoding]::UTF8.GetString($scriptBytes)
-[ScriptBlock]::Create($installerScript).Invoke()
+try {
+    & $tempPath
+} finally {
+    if (Test-Path -LiteralPath $tempPath) {
+        Remove-Item -LiteralPath $tempPath -Force
+    }
+}
